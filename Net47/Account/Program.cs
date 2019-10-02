71d8eb12d2a437ec35a0c9d6632847393f25e03a
Ottó Fukszius <fuksziuso@outlook.com>
2019-10-02 20:58:55 +0200

    Account move to NET47

---
+++ Net47/Account/Program.cs
@@ -0,0 +1,31 @@
+﻿// Each account on a website has a set of access flags that represent a users access.
+//   
+// Update and extend the enum so that it contains three new access flags:
+//   
+//  1. A Writer access flag that is made up of the Submit and Modify flags.
+//  2. An Editor access flag that is made up of the Delete, Publish and Comment flags.
+//  3. An Owner access that is made up of the Writer and Editor flags.
+
+
+using System;
+
+public class Account
+{
+    [Flags]
+    public enum Access
+    {
+        Submit = 1,
+        Modify = 2,
+        Writer = 3,
+        Delete = 8,
+        Publish = 16,
+        Comment = 32,
+        Editor = 56,
+        Owner = 59
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Access.Writer.HasFlag(Access.Delete)); //Should print: "False"
+    }
+}
