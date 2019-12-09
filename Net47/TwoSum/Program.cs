9c2bb1a4efb36331cae41c7a922420b9e86dce22
sentrada <sentrada@gmail.com>
2019-12-09 16:51:16 +0100

    fix namespace

--- Net47/TwoSum/Program.cs
+++ Net47/TwoSum/Program.cs
@@ -1,33 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class TwoSum
+namespace TwoSum
 {
-    public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
+    class TwoSum
     {
-        var dictionary = new Dictionary<int,int>();
-
-        for (int i = 0; i < list.Count; i++)
+        public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
         {
-            var value = list[i];
-            var required = sum - value;
+            var dictionary = new Dictionary<int,int>();
 
-            if (dictionary.ContainsKey(required))
-                return new Tuple<int, int>(i,dictionary[required]);
+            for (int i = 0; i < list.Count; i++)
+            {
+                var value = list[i];
+                var required = sum - value;
 
-            if(!dictionary.ContainsKey(value))
-                dictionary.Add(value, i);
+                if (dictionary.ContainsKey(required))
+                    return new Tuple<int, int>(i,dictionary[required]);
+
+                if(!dictionary.ContainsKey(value))
+                    dictionary.Add(value, i);
+            }
+
+            return null;
         }
 
-        return null;
-    }
-
-    public static void Main(string[] args)
-    {
-        Tuple<int, int> indices = FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10);
-        if (indices != null)
+        public static void Main(string[] args)
         {
-            Console.WriteLine(indices.Item1 + " " + indices.Item2);
+            Tuple<int, int> indices = FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10);
+            if (indices != null)
+            {
+                Console.WriteLine(indices.Item1 + " " + indices.Item2);
+            }
         }
     }
 }
